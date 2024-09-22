f5973a442144179cd56f039fc7a16ea6fcab6d7d
adil-16 <adilmasood833@gmail.com>
2024-09-22 21:02:39 +0500

    Footer Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,55 +1,76 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FaFacebookF, FaTwitter, FaLinkedin } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
-import { SiWhatsapp } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <footer className="bg-white shadow-lg py-12">
-      <div className="container mx-auto px-4 grid lg:grid-cols-3 gap-8">
+    <footer className="bg-white py-12">
+      <div className="container mx-auto px-4 grid lg:grid-cols-4 gap-8">
         <div className="space-y-4 text-center lg:text-left">
           <img
             src="/logo.png"
             alt="Logo"
-            className="mx-auto lg:mx-0 h-12 w-auto"
+            className="mx-auto lg:mx-0 h-20 w-auto"
           />
-          <p className="text-gray-600 text-sm">
+          <p className="text-custom-gray text-sm">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Alquam.
           </p>
           <div className="flex justify-center lg:justify-start space-x-4">
-            <FaFacebookF className="text-gray-500 hover:text-blue-500" />
-            <RiInstagramFill className="text-gray-500 hover:text-pink-600" />
-            <FaTwitter className="text-gray-500 hover:text-blue-400" />
-            <FaYoutube className="text-gray-500 hover:text-red-600" />
-            <SiWhatsapp className="text-gray-500 hover:text-green-500" />
+            <FaFacebookF className="text-custom-blue hover:text-blue-500" />
+            <FaTwitter className="text-custom-blue hover:text-blue-400" />
+            <RiInstagramFill className="text-custom-blue hover:text-pink-600" />
+            <FaLinkedin className="text-custom-blue hover:text-blue-500" />
           </div>
         </div>
 
         <div className="space-y-4">
-          <h3 className="font-bold text-gray-800">Quick Links</h3>
-          <ul className="space-y-2 text-gray-600">
+          <h3 className="font-bold text-custom-black text-lg">Products</h3>
+          <ul className="space-y-2 text-custom-gray">
             <li>
               <Link to="/">Features</Link>
             </li>
             <li>
-              <Link to="/">Pricing</Link>
+              <Link to="/">Testimonials</Link>
             </li>
             <li>
-              <Link to="/">Support</Link>
+              <Link to="/pricing">Pricing</Link>
             </li>
             <li>
-              <Link to="/">Privacy Policy</Link>
+              <Link to="/">FAQs</Link>
             </li>
             <li>
-              <Link to="/">Terms of Service</Link>
+              <Link to="/">Home</Link>
             </li>
           </ul>
         </div>
 
         <div className="space-y-4">
-          <h3 className="font-bold text-gray-800">Contact Information</h3>
-          <ul className="space-y-2 text-gray-600">
+          <h3 className="font-bold text-custom-black text-lg">Quick Links</h3>
+          <ul className="space-y-2 text-custom-gray">
+            <li>
+              <Link to="/">About Us</Link>
+            </li>
+            <li>
+              <Link to="/">Services</Link>
+            </li>
+            <li>
+              <Link to="/privacyPolicy">Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to="/">Terms of Service</Link>
+            </li>
+            <li>
+              <Link to="/">Lorem</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="space-y-4">
+          <h3 className="font-bold text-custom-black text-lg">
+            Contact Information
+          </h3>
+          <ul className="space-y-2 text-custom-gray">
             <li>
               Email:{" "}
               <a href="mailto:info@cma-reports.com">info@cma-reports.com</a>
@@ -60,14 +81,10 @@ const Footer = () => {
         </div>
       </div>
       <div className="border-t border-gray-200 mt-8 pt-4">
-        <div className="container mx-auto px-4 flex flex-col lg:flex-row justify-between text-sm text-gray-600">
+        <div className="container mx-auto px-4 flex flex-col lg:flex-row justify-center text-sm text-custom-gray">
           <p className="text-center lg:text-left">
             &copy; 2023 CMA Reports. All rights reserved.
           </p>
-          <div className="flex justify-center lg:justify-end space-x-4">
-            <Link to="/terms-of-service">Terms of Service</Link>
-            <Link to="/privacy-policy">Privacy Policy</Link>
-          </div>
         </div>
       </div>
     </footer>
