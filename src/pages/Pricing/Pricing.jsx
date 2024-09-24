a6d86c396541e9e7fb724f21a18bb50990edf16a
adil-16 <adilmasood833@gmail.com>
2024-09-24 12:27:15 +0500

    Pricing colors updation

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -55,13 +55,13 @@ const Pricing = () => {
         {(isEmail ? emailPlanData : planData).map((plan, idx) => (
           <div
             key={idx}
-            className={`${plan.bgClass} shadow-xl h-auto p-10 rounded-xl relative`}
+            className={`${plan?.bgClass} shadow-xl h-auto p-10 rounded-xl relative`}
           >
             {plan.isProfessional || plan.isPopular ? (
               <div
                 className={`absolute top-8 ${
                   isEmail ? "left-56" : "left-60"
-                } transform -translate-x-1/2 border text-sm text-white px-3 py-1 rounded-lg`}
+                } transform -translate-x-1/2 bg-indigo-300 text-sm text-white px-3 py-1 rounded-lg`}
               >
                 {plan.isProfessional || plan.isPopular ? "Popular" : ""}
               </div>
